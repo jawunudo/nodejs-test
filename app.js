5871b9c002a4c8e1f3f43eff928680d5b7d23ecc
Justine Awunudo <jawunudo@gmail.com>
2024-05-28 11:34:05 +0100

    test

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const _ = require("lodash")
-
-const item = [1, [2, [3, [4]]]]
-const newItem = _.flattenDeep(item)
-console.log(newItem)
+setInterval(() => {
+  console.log("Hello World")
+}, 2000)
+console.log("I will run first")
+// Process stays alive unless
+// Kill Process CTRL + C
+// unexpected error
